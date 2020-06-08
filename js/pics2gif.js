76fdeaa3f622a7d256bb83a7c7cfffd31ff3b02d
Michael Schwartz <mikethedj4@yahoo.com>
2020-06-08 00:01:04 -0500

    Added a link to instructional video

--- js/pics2gif.js
+++ js/pics2gif.js
@@ -1,3 +1,19 @@
+// Show Lightbox Video Onload
+var playGuide = function() {
+  $.fancybox.open({
+    youtube : {
+      controls : 0,
+      showinfo : 0
+    },
+    src  : 'https://www.youtube.com/watch?v=ZVQaiXamOSU', // Source of the content
+    type : 'iframe', // Content type: image|inline|ajax|iframe|html (optional)
+    opts : {}
+  });
+};
+
+// Watch How To Video
+alertify.message("<div class=\"grid\"><div class=\"centered grid__col--12 tc\"><h2>Instructional Guide!</h2><a class=\"pointer\" onclick=\"playGuide()\"><img src=\"imgs/playvideo.svg\" width=\"50%\"></a></div></div>");
+
 // load svg file
 function previewFiles() {
   var files   = openfile.files;
